3369a800eff7083ba77039a8addd7091479e24cd
F Claude <52762083+whoiscoos@users.noreply.github.com>
2021-05-01 15:14:31 -0600

    update flameshot config and kill monitor script

--- .config/i3/scripts/lock/i3_kill_monitors.sh
+++ .config/i3/scripts/lock/i3_kill_monitors.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
 
-LOCKSCRIPT='/home/fclaude/.config/i3/scripts/lock/i3_pixel_lock.sh'
+# sleep 2 seconds
+/usr/bin/sleep 2s
 
-revert_dpms() {
-
-    # force monitors to activate
-    /usr/bin/xset dpms force on     
-
-    # sets dpms to default values (seconds)
-    # (standby | suspend | off)
-
-    /usr/bin/xset +dpms dpms 3600 3600 3600
-}
-
-# set trap for revert_dpms function
-trap revert_dpms HUP INT TERM
-
-# kill monitors
-/usr/bin/sleep 1.5
+# force dpmks to kill monitors
 /usr/bin/xset dpms force off
-
-# revert dpms values
-revert_dpms
